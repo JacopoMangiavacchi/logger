627bc600af41a8a8d0bf7bcaedc2678676589669
Jacopo <Jacopo@jMacBookPro-2.local>
2017-04-02 10:02:40 +0100

    Fix generic log support

--- server/nodejs/routes.js
+++ server/nodejs/routes.js
@@ -19,35 +19,10 @@ router.route('/log/:template')
         // Save log and check for errors
         switch (req.params.template) {
             case 'simple':
-                console.log("validating simple ...")
-
-                var SimpleSchema = require('./models/simple');
-                var simple = new SimpleSchema();
-                simple.simpleValue = req.body.simpleValue
-                simple.payload = req.body.payload;
-
-                var error = simple.validateSync();
-                if (error == null) {
-                    console.log("saving simple ...")
-                    simple.save(function(err) {
-                        if (err) {
-                            console.log('Error saving: ', err);
-                            res.send(err);
-                        }
-                        else {
-                            console.log('Saved');
-                            res.json({ message: 'Log created successfully!' });
-                        }
-                    });
-                }
-                else {
-                    console.log('Error validating: ', error);
-                    saveGenericLog(req.params.template, req.body, res)
-                }
+                trySaveSimpleLog(req.params.template, req.body, res);
                 break;
         
             default:
-                console.log("saving generic ...")
                 saveGenericLog(req.params.template, req.body, res)
                 break;
         }
@@ -56,9 +31,11 @@ router.route('/log/:template')
 module.exports = router;
 
 function saveGenericLog(template, body, res) {
+    console.log("saving generic ...")
+
     var GenericSchema = require('./models/generic');
     var generic = new GenericSchema();
-    generic.template template;
+    generic.template = template;
     generic.body = body;
 
     generic.save(function(err) {
@@ -73,6 +50,33 @@ function saveGenericLog(template, body, res) {
     });
 }
 
+function trySaveSimpleLog(template, body, res) {
+    console.log("validating simple ...")
+
+    var SimpleSchema = require('./models/simple');
+    var simple = new SimpleSchema();
+    simple.simpleValue = body.simpleValue
+    simple.payload = body.payload;
+
+    var error = simple.validateSync();
+    if (error == null) {
+        console.log("saving simple ...")
+        simple.save(function(err) {
+            if (err) {
+                console.log('Error saving: ', err);
+                res.send(err);
+            }
+            else {
+                console.log('Saved');
+                res.json({ message: 'Log created successfully!' });
+            }
+        });
+    }
+    else {
+        console.log('Error validating: ', error);
+        saveGenericLog(template, body, res)
+    }
+}
 
 function dateDisplayed(timestamp) {
     var date = new Date(timestamp);
